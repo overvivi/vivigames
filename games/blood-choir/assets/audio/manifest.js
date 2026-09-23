84b049b014af2ae508d2181f8fa0d5a0dbb9ba85
vivi <vivinet2525@gmail.com>
2026-09-24 00:22:55 +0900

    Balance enemy death audio with other sound effects

--- games/blood-choir/assets/audio/manifest.js
+++ games/blood-choir/assets/audio/manifest.js
@@ -1,4 +1,4 @@
 // Sound Workshopの書出しを同じフォルダーに置き、nullを相対URLへ変更する。
 // 例: shot: 'assets/audio/shot.wav'。未指定の音は仮の合成音を使う。
-window.BCSoundManifest={shot:null,hit:null,death:'assets/audio/enemy-death-05.wav',hurt:null,jump:null,dash:null,lightning:null,clear:null,boss:null,bossPhase:null,bellWarning:null,bellPulse:null,sweepWarning: null, sweepErupt: null,
+window.BCSoundManifest={shot:null,hit:null,death:'assets/audio/enemy-death-05.wav?v=20260924',hurt:null,jump:null,dash:null,lightning:null,clear:null,boss:null,bossPhase:null,bellWarning:null,bellPulse:null,sweepWarning: null, sweepErupt: null,
   pillarWarning:null,pillarErupt:null,victory:null,combo:null,evolve:null,ultimate:null,ultimate_lantern:null,ultimate_needle:null,ultimate_censer:null,ultimate_bell:null,ultimate_book:null,ultimate_scythe:null,revive:null,dead:null,select:null,enemyShot:null,cleave:null,burst:null,plagueBurst:null,heal:null,shield:null,summon:null,danger:null,wave:null,ambience:null};
